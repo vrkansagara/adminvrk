80640f02c83327db481366716d40a1ac63af0995
Vallabh Kansagara <vrkansagara@gmail.com>
2016-09-07 15:09:49 +0530

    project files are added.

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vallabh
+ * Date: 9/7/16
+ * Time: 3:05 PM
+ */
+?>
+<script src="assets/js/vendor/jquery.js"></script>
+<script src="assets/js/vendor/what-input.js"></script>
+<script src="assets/js/vendor/foundation.js"></script>
+<script src="assets/js/app.js"></script>
+<script>
+    $(document).foundation();
+</script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <noscript>
+        For full functionality of this site it is necessary to enable JavaScript.
+        Here are the <a href="http://www.enable-javascript.com/" target="_blank"> instructions how to enable JavaScript
+            in your web browser</a>.
+    </noscript>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+<body>
+
+
